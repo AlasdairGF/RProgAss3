a3b8067899c28a2b4b2a00edf5a179619a879960
AlasdairGF <alasdair@gordon-finlayson.net>
2014-06-29 05:04:58 +0100

    So close - no passing test 9, all else passes

--- rankall.R
+++ rankall.R
@@ -2,16 +2,16 @@
 ## Alasdair GF
 ## https://github.com/AlasdairGF/RProgAss3
 
-## Part Three
-rankhospital <- function(state, outcome, num = "best") {
+## Part Four
+rankall <- function(outcome, num = "best") {
       ## Read outcome data
       ## Check that state and outcome are valid
-      ## Return hospital name in that state with the given rank
-      ## 30-day death rate
+      ## For each state, find the hospital of the given rank
+      ## Return a data frame with the hospital names and the
+      ## (abbreviated) state name
       
       # Read data & validate query
       fulldata <- read.csv("outcome-of-care-measures.csv", na.strings="Not Available")
-      if (!state %in% data$State) stop("invalid state") 
       if (!outcome %in% c("heart attack", "heart failure", "pneumonia")) stop("invalid outcome")
       
       # Select out only the data needed for this problem...
@@ -24,15 +24,23 @@ rankhospital <- function(state, outcome, num = "best") {
       }
       names(data) <- c("Hosp","State","rate")
       data<-data[complete.cases(data$rate),] # strip out all rows where rate is NA
-      data<-data[data$State == state,] # choose only relev state
       
-      # order data
-      data <- data[order(data$rate,data$Hosp),]
+      # Initialise empty data.frame for results
+      results <- data.frame(hospital=character(),state=character(),stringsAsFactors=FALSE)
+      
+      states <- levels(factor(data$State)) # vector of states
       
       # translate "best" and "worst" to row numbers
       if (num=="best") num <- 1
       if (num=="worst") num <- nrow(data)
-
-      # Return relevant hosp name
-      as.character(data[num,1])
+      
+      for (loop in seq(length(states))) {
+            tempData <- data[data$State == states[loop],] # select each state in turn
+            tempData <- tempData[order(tempData$rate,tempData$Hosp),] # order it
+            newRow <- data.frame(hospital=as.character(tempData$Hosp[num]),state=as.character(tempData$State[num]))
+            colnames(newRow)<-colnames(results)
+            results <- rbind(results,newRow) # add
+      }
+      colnames(results)<-c("hospital","state")
+      results
 }
